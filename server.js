46ab13056cc5a6842ad7b28318a46ff9e83dcb7a
Rahul Gupta <useritch96@gmail.com>
2017-10-13 21:07:12 +0530

    Added passport and google strategy

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 'use strict'
-
+//-----------LIBRARIES---------------
 const express = require('express');
 let app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-const PORT = process.env.PORT || 8080;
+//-------------FILES-----------------
+let secret = require('./config/secret');
 
+//----------MIDDLEWARES--------------
+passport.use(new GoogleStrategy());
+
+//------------ROUTES---------------
 app.get('/', (req, res) => {
   res.send('Hello there again');
 });
 
-app.listen(PORT, (err) => {
+//------------LISTENING------------
+app.listen(secret.PORT, (err) => {
   if(err) console.log(err);
   console.log(`Server running on port ${PORT}`);
 });

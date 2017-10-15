ed45d4e6194fae5e657aebede24401e871e02b6f
Rahul Gupta <useritch96@gmail.com>
2017-10-15 21:41:34 +0530

    Refactored

--- server.js
+++ server.js
@@ -2,22 +2,28 @@
 //-----------LIBRARIES---------------
 const express = require('express');
 let app = express();
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
 
 //-------------FILES-----------------
 let secret = require('./config/secret');
+let User = require('./models/User');
+require('./services/passport');
+let authRoutes = require('./routes/authRoutes');
+
+mongoose.connect(secret.mongoURI, (err) => {
+  if(err) {console.log(err);}
+  else {console.log('Database Connected');}
+});
 
 //----------MIDDLEWARES--------------
-passport.use(new GoogleStrategy());
+
 
 //------------ROUTES---------------
-app.get('/', (req, res) => {
-  res.send('Hello there again');
-});
+app.use(authRoutes);
 
 //------------LISTENING------------
 app.listen(secret.PORT, (err) => {
   if(err) console.log(err);
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${secret.PORT}`);
 });

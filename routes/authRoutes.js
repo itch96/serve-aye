2379e40be2abddb2901dcb6af8e66e3e2cd4d3db
Rahul Gupta <useritch96@gmail.com>
2017-10-15 21:39:57 +0530

    Seperated the routes from the server file

---
+++ routes/authRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+let router = express.Router();
+const passport = require('passport');
+
+router.get(
+  '/auth/google', 
+  passport.authenticate('google', {scope: ['profile', 'email']}),
+);
+
+router.get(
+  '/auth/google/callback', 
+  passport.authenticate('google')
+);
+
+module.exports = router;
